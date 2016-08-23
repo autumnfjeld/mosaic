ab5922f27ebade718c465c7ed6ffa93084e94f35
Autumn Fjeld <autumn@talentbin.com>
2016-08-22 22:07:34 -0700

    wip

--- js/mosaic-maker.js
+++ js/mosaic-maker.js
@@ -1,6 +1,5 @@
 (function(){
 	'use strict';
-	console.log('running mosaic constructor');
 	/**
 	 * Mosaic module to create mosaic from image
 	 * @constructor
@@ -10,11 +9,15 @@
 		if (!userOptions.sourceImg) {
 			throw new Error('source image not defined');
 		}
-		// this.setImage(userOptions.sourceImg);
+
 		this.mergeOptions(userOptions);
 
-		//make sure image is uploaded before calling
-		this.init();
+		//ensure sure image is loaded before processing
+		if (this.options.sourceImg.complete) {
+			this.init();
+		} else {
+			this.options.sourceImg.onload = this.init.bind(this);
+		}
 	}
 
 	//TODO After upload use new Image() to set image ****
@@ -55,36 +58,69 @@
 		// console.log('this.options.sourceImg', this.options.sourceImg);
 		this.options.canvasWidth = this.options.sourceImg.naturalWidth;
 		this.options.canvasHeight = this.options.sourceImg.naturalHeight;
-		//Find number of tiles in x & y directions
 		this.options.tilesX = Math.floor(this.options.canvasWidth / this.options.tileWidth);
 		this.options.tilesY = Math.floor(this.options.canvasHeight / this.options.tileHeight);
 
-		this.drawImageOnCanvas();
+		this.getTileColors();
 		console.log('end', this);
 	};
 
-	Mosaic.prototype.drawImageOnCanvas = function(){
-		var img = this.options.sourceImg,
-		    w = this.options.canvasWidth,
-		    h = this.options.canvasHeight,
-		    canvas = document.createElement('canvas'),
-		    context = canvas.getContext('2d');    
-
-		context.drawImage(img, 0, 0);
-
-		var imageData = context.getImageData(0, 0, 16, 16);
-		console.log('imageData', imageData.data);
+	Mosaic.prototype.getTileColors = function(){
 		// debugger;
-		//test
-		var test = document.getElementById('testshowcanvas');
-		test.appendChild(canvas);
+		var options = this.options,
+				tileSize = this.options.tileWidth,   	//assumes tiles will be square or circle  
+				xPixels = options.tileWidth,  				//number of pixels in a tile in x direction
+				yPixels = options.tileHeight;  				//number of pixels in a tile in y direction
+		    imageData = this.getImageData(), 			// [r1,g1,b1,a1,r2,g2,b2,a2]
+		    colorData = imageData.data,
+		    imageWidth = imageData.width;
 
-		// canvas.width = this.options.tileWidth * this.options.
+		for (var j=0; j < options.tilesY; j++){  //iterate thru rows of tiles
+		  for (var i=0; i < options.tilesX; i++) {  //iterate thru columns of tiles
+				var x = i * options.tileWidth,  		// x pixel position in canvas 16
+				    y = j * options.tileHeight; 		// y pixel position in canvas
+				this.getTileAvgRGB(x, y, xPixels, yPixels, imageWidth, colorData);
+				//send color back to controller to fetch svg from server
+			}
+		}
+	}
 
+	/**
+	 * Gets the r,g,b,a data for the pixels in a tile where x,y represent (0,0) of tile coords
+	 * @param  {integer} x  			 x pixel position in canvas, min x in tile
+	 * @param  {integer} y  			 y pixel position in canvas, min y in tile tile
+	 * @param  {integer} xPixels	 num of pixels in tile in x  
+   * @param  {integer} yPixels   num of pixels in tile in y 
+	 * @param  {integer} imgWidth  width (height) dimension of canvas
+	 * @param  {array} data  			 rgba data in format [r1,g1,b1,a1,r2,g2,b2,a2]
+	 * @return {array}   					 rgbdata [[r1,g1,b1,a1], [r2,g2,b2,a2],...] of size tileWidthxtileHeight
+	 */
+	Mosaic.prototype.getTileAvgRGB = function(x, y, xPixels, yPixels, imgWidth, data){
+		//will go over 16x16 pixels > data array is 16x16x4 
+		// debugger;
+		var tileData = [],  //length 256
+				pixelData = [],
+				rgbSums = {r: 0, g: 0, b: 0},
+				avgRGB = {};
+
+		// this.skipPixels = 0;
+		//iterate over every pixel, summing the r, g, & b
+		for (var row=y; row < (y + yPixels); row++ ){			 			//iterate over pixel rows
+			for (var col=x; col < (x + xPixels); col++) {					//iterate over cols
+				// console.log('array index kind of', (row*imgWidth + col)*4);
+				// console.log('data point ', data[(row*imgWidth + col)*4 + 0]);
+				rgbSums['r'] += data[(row*imgWidth + col)*4 + 0];
+				rgbSums['g'] += data[(row*imgWidth + col)*4 + 1];
+				rgbSums['b'] += data[(row*imgWidth + col)*4 + 2];
+				tileData.push(pixelData);  //will be an array of 16x16
+			}
+		} 
+			for (var prop in rgbSums){
+				avgRGB[prop] = Math.floor(rgbSums[prop]/(xPixels*yPixels));
+			}
+			console.log('getTileAvgRGB: ', avgRGB);
+			return avgRGB;
 	};
-
-
-
 
 
 	window.app = window.app || {};
