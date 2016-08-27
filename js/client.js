d7148c22b528dac11c2b7fdea93be889a232c26e
Autumn Fjeld <autumn@talentbin.com>
2016-08-27 16:09:19 -0700

    Return promise from imagemodel.init and send canvas to web-worker

--- js/client.js
+++ js/client.js
@@ -1,13 +1,27 @@
 
 (function(){
 	'use strict';
-	console.log('running ViewController')
+
 	/**
 	 * ViewController module for orchestrating view & mosaic model
 	 * @constructor
 	 */
 	function ViewController(){
+		console.log('init ViewController');
+		this.imageEl = null;
+
+		this.tile = {
+			width:  window.TILE_WIDTH,
+			height: window.TILE_HEIGHT
+		};
+
+		// Init models
+		this.imageModel = null;
+		this.mosaicModel = [];  //array of objects {1: hex1, 2: hex2}
 		
+		// var testImage =  document.getElementById('source-image');
+		// var maker = new app.Mosaic({  'sourceImg': testImage});
+
 	}
 
 	/**
@@ -15,17 +29,44 @@
 	 * @param  {[type]} image [description]
 	 * @return {[type]}       [description]
 	 */
-	ViewController.prototype.loadImage = function(image){
-		document.getElementById('source-image').src = image;
-		console.log('ViewController.loadImage', image);
-		// debugger;
-		// CALL WEBWORKER
-		var mosaic = new window.app.Mosaic({
-			'sourceImg': document.getElementById('source-image'),
-      'mosaicEl':  document.getElementById('mosaic') //move this 
-		});
+	ViewController.prototype.processImage = function(image){
+		// document.getElementById('source-image').src = image;
+		this.imageEl = document.getElementById('source-image');
+		// console.log('ViewController.loadImage', this.imageEl);
+
+		this.imageModel = new app.ImageModel(this.imageEl);		
+		this.imageModel.init().then(function(res){
+			// console.log('res', res);
+		  // console.log('From ViewController Instance:  this.imageModel', this.imageModel.canvas);
+		  this.makeMosaic(this.imageModel.canvas);
+			
+		}.bind(this));  //NEED a promise here?
+		//TODO: process rejection
+
 	};
 
+	ViewController.prototype.makeMosaic = function(canvas){
+		// debugger;
+		console.log('canvas', canvas);
+		// debugger;
+		var mosaicWorker = new Worker('js/mosaic-worker.js');
+		mosaicWorker.postMessage(canvas.data.buffer);
+		mosaicWorker.onmessage = function(e){
+			console.log('Message from worker');
+			//	 this.hexColorsByRow.push(row);
+			//	 this.getSvgTiles
+		}
+		//make sure to send a special termination msg
+	};
+
+	ViewController.prototype.getSvgTiles = function(){
+		//call server
+		//on promise completed call this.display
+	};
+
+	ViewController.prototype.display = function(){
+
+	};
 
 	window.app = window.app || {};
 	window.app.ViewController = ViewController;
@@ -34,18 +75,22 @@
 
 
 
-
 (function(){
 	'use strict';
 	/**
-	 * Namespace
+	 * Namespace constructor
+	 * @constructor
 	 */
 	function MosaicApp(){
 		this.vc = new app.ViewController();
+		// this.model = new app.ImageData();
+		// this.worker = new Worker('mosaic-maker.js')
+		
 	}
 	
 	window.mosaicApp = new MosaicApp();
-	console.log('mosaicApp', window.mosaicApp);
+	// console.log('mosaicApp', window.mosaicApp);
+	window.mosaicApp.vc.processImage();  //for dev
 
 })();
 
---
+++ js/mosaic-worker.js
@@ -0,0 +1,12 @@
+onmessage = function(e){
+  console.log('message received', e);
+		// var mosaic = new window.app.Mosaic({
+		// 	'sourceImg': document.getElementById('source-image'),
+  //     'mosaicEl':  document.getElementById('mosaic') //move this 
+		// });  
+}
+
+
+function Mosaic(){
+	
+}
