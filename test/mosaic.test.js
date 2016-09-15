2714665895ccfdd23838539b5a48f439495c821e
Autumn Fjeld <autumn@talentbin.com>
2016-09-14 20:20:49 -0700

    Update tests for mosaic worker functions

--- test/mosaic.test.js
+++ test/mosaic.test.js
@@ -2,87 +2,91 @@ var expect = chai.expect;
 
 
 describe('App', function(){
+
    it('should exist', function() {
         expect(window.app).to.exist;
     });
+
 });
 
 
 describe('ImageModel Constructor ', function() {
 
-  describe('initilization', function(){
-
     var image = new Image();
     image.crossOrigin = 'Anonymous';
     image.src = getSource();
     image.complete = true;  //not working
-    // debugger;
     var model = new app.ImageModel(image);
     console.log('mooooodle', model);
+
 
     it('should accept an image', function(){
       expect(model.srcImage.imgEl).to.eql(image);
     });
 
+
     it('should throw an error if original image is not defined', function() {
-      console.log('**@@@*', app.ImageModel);
-      // var model2 = new app.ImageModel();
-      // expect(model2).to.throw(TypeError);
-      expect(function(){new app.ImageModel()}).to.throw('Error: source image not defined');
+      expect(function(){new app.ImageModel()}).to.throw('source image not defined');
     });    
-    
+
+});
+
+describe('Mosaic Worker', function() {
+
+  it('should exist', function(){
+    expect(mosaicWorker).to.exist;
+  });
+
+
+  it('should correctly compute average rgb', function(){
+    var imageData = [ 255,0,246,1 , 214,0,207,1 , 155,30,204,1 , 51,30,204,1];
+    var avgRGB = {r:0,g:0,b:0};
+
+    for (i = 0; i < imageData.length; i+=4){
+      avgRGB.r += imageData[i];
+      avgRGB.g += imageData[i+1];
+      avgRGB.b += imageData[i+2];
+    }
+
+    for (prop in avgRGB){
+      avgRGB[prop] = Math.floor(avgRGB[prop]/(imageData.length/4));
+    }
+
+    expect(getTileAvgRGB(0, 0, 2, 2, 2, imageData)).to.eql(avgRGB);
+  });    
+
+
+  it('should correctly compute hex from rgb', function(){
+    var rgb1 = {r: 51, g: 30, b: 204},
+        rgb2 = {r: 155, g: 30, b: 204};
+
+    expect(rgbToHex(rgb1)).to.eql('331ecc');
+    expect(rgbToHex(rgb2)).to.eql('9b1ecc');
   });
 
 });
 
-// describe('Mosaic Maker', function() {
 
-//   describe('initilization', function(){
+// describe('Mosaic.prototype.getTileAvgRGB', function(){
 
-//     var image = new Image(),
-//         test = document.getElementById('test-image');    
-//     image.crossOrigin = 'Anonymous';
-//     image.src = getSource();
-//     //verify image: ie test the test
-//     test.appendChild(image);
-//     debugger;
-//     var mosaic = new app.Mosaic({
-//       'sourceImg': image
-//     });
+//   it('should correctly compute average rgb', function(){
+//     var imageData = [ 255,0,246,1 , 214,0,207,1 , 155,30,204,1 , 51,30,204,1];
+//     var avgRGB = {r:0,g:0,b:0};
 
-//     it('should accept an image', function(){
-//       console.log('mosaid in tst', mosaic);
-//       expect(mosaic.options.sourceImg).to.eql(image);
-//     });
-
-//     it('should throw an error if original image is not defined', function() {
-//         expect(app.Mosaic).to.throw(TypeError);
-//     });    
-    
+//     for (i = 0; i < imageData.length; i+=4){
+//       avgRGB['r'] += imageData[i];
+//       avgRGB['g'] += imageData[i+1];
+//       avgRGB['b'] += imageData[i+2];
+//       // console.log('avgRGB', avgRGB);
+//     }
+//     for (prop in avgRGB){
+//       avgRGB[prop] = Math.floor(avgRGB[prop]/(imageData.length/4));
+//     }
+//     console.log('avgRGB', avgRGB);
+//     expect(app.Mosaic.prototype.getTileAvgRGB(0, 0, 2, 2, 2, imageData)).to.eql(avgRGB);
 //   });
-
-
-//   describe('Mosaic.prototype.getTileAvgRGB', function(){
-
-//     it('should correctly compute average rgb', function(){
-//       var imageData = [ 255,0,246,1 , 214,0,207,1 , 155,30,204,1 , 51,30,204,1];
-//       var avgRGB = {r:0,g:0,b:0};
-
-//       for (i = 0; i < imageData.length; i+=4){
-//         avgRGB['r'] += imageData[i];
-//         avgRGB['g'] += imageData[i+1];
-//         avgRGB['b'] += imageData[i+2];
-//         // console.log('avgRGB', avgRGB);
-//       }
-//       for (prop in avgRGB){
-//         avgRGB[prop] = Math.floor(avgRGB[prop]/(imageData.length/4));
-//       }
-//       console.log('avgRGB', avgRGB);
-//       expect(app.Mosaic.prototype.getTileAvgRGB(0, 0, 2, 2, 2, imageData)).to.eql(avgRGB);
-
-//     });
-    
-//   });
+  
+// });
 
 //   describe('Mosaic.prototype.rgbToHex', function(){
 
